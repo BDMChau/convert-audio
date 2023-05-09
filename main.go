4df854c107a56dd49c5cf4cb58c9aca51f3a429c
chau <chau.bui@codestringers.com>
2023-05-09 20:24:16 +0700

    first commit

--- main.go
+++ main.go
@@ -2,42 +2,59 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
-func handleReadFiles(f *os.File) {
+var convertTo = ".mp3"
+
+const maxGoroutines = 10
+
+func handleConvert(file fs.FileInfo, guard <-chan struct{}) {
+	pathFile := "./music/" + file.Name()
+
+	splitName := strings.Split(file.Name(), ".")
+	splitName[len(splitName)-1] =
+		strings.Replace(splitName[len(splitName)-1], splitName[len(splitName)-1], convertTo, -1)
+
+	newPathFile := "./output/" + strings.Join(splitName, "")
+
+	err := exec.Command("ffmpeg", "-i", pathFile, "-ab", "320k", "-map_metadata", "0", "-id3v2_version", "3", newPathFile).Run()
+	if err != nil {
+		fmt.Println("Error:", file.Name(), err.Error())
+	}
+
+	<-guard
+	fmt.Println("OK:", file.Name())
+
+}
+
+func handleReadFiles(f *os.File, wg *sync.WaitGroup, guard chan struct{}) {
 	files, err := f.Readdir(0)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	convertTo := ".mp3"
-
 	for _, file := range files {
-		fileName := "./music/" + file.Name()
-		splitName := strings.Split(file.Name(), ".")
-		splitName[len(splitName)-1] =
-			strings.Replace(splitName[len(splitName)-1], splitName[len(splitName)-1], convertTo, -1)
+		guard <- struct{}{}
+		wg.Add(1)
+		go handleConvert(file, guard)
 
-		newFileName := strings.Join(splitName, "")
-
-		// ffmpeg -i input.mp3 output.flac
-		cmd := "ffmpeg -i " + fileName + " " + newFileName
-
-		err = exec.Command(cmd).Run()
-		if err != nil {
-
-		}
-
-		fmt.Println(exec.Command("ffmpeg -i ", "D:/School/Web/Go/convert-audio/music/04._Work_it_out.flac", " 04_Work_it_out.mp3").Run())
+		defer wg.Done()
 	}
+	wg.Wait()
+
 }
 
 // //////////////
 func main() {
+	wg := new(sync.WaitGroup)
+	guard := make(chan struct{}, maxGoroutines) // to limit Go-routines
+
 	f, err := os.Open("./music")
 
 	if err != nil {
@@ -46,5 +63,5 @@ func main() {
 		return
 	}
 
-	handleReadFiles(f)
+	handleReadFiles(f, wg, guard)
 }
